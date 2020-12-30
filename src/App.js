961c9c857f162fee6fd77181d7aa913275c3c1d7
Aot <aot_aln32@hotmail.com>
2020-12-30 21:50:42 +0700

    add removeTodo

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Body from "./body";
 import SubmitForm from "./submitform";
 import {useState} from "react"
 
+
 function App() {
   const [todos ,setTodos] = useState(["1", "2", "3",'4']);
+
+  function removeTodo(word) {
+    var newTodos = todos.filter((_) => _ !== word)
+    setTodos(newTodos)
+  }
 
   function addTodo(_){
     console.log(_)
@@ -18,7 +24,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Title/>
-        <Body todos={todos}/>
+        <Body todos={todos} removeTodo = {(e) => removeTodo(e)}/>
         <SubmitForm addTodo= {(e) => addTodo(e)}/>
       </header>
     </div>
--- src/body.js
+++ src/body.js
@@ -1,7 +1,18 @@
 function Body(props) {
 
+    function removeMe(word){
+        console.log(word)
+    }
+
     return (<ul>
-        {props.todos.map((e) => (<li key={e}>{e}</li>))}
+        {props.todos.map((e) =>
+        (
+            <li key={e}>
+                {e} &nbsp; &nbsp; &nbsp;
+                <span onClick={()=> props.removeTodo(e)}>X</span>
+            </li>
+        )
+        )}
     </ul>)
 }
 
